74af0d5b65a886ad5dd16fba8a12e7a58e4b2b32
Miles Turin <milesturin@gmail.com>
2021-12-03 21:09:31 -0800

    This commit adds capability to use k-means to pick thread color

--- seamster.py
+++ seamster.py
@@ -2,11 +2,13 @@ import sys, argparse, io
 from math import sin, cos, tau
 from PIL import Image, ImageStat, ImageDraw, ImageChops
 import numpy as np
+from sklearn.cluster import KMeans
 from bresenham import bresenham
 
 IMAGE_DIR = 'images/'
 RESULT_PATH = 'out.jpg'
 INSTRUCTIONS_PATH = 'instructions.txt'
+MASK_COLOR = np.array([255, 0, 255])
 RESULT_SIZE = 5000
 RESULT_SIZE = (RESULT_SIZE, RESULT_SIZE)
 NEAR_CULL = 10
@@ -20,11 +22,9 @@ def calculate_nail_coord(nail, total_nails, image_size):
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', metavar='iterations', type=int, default=5000, help='number of iterations')
 parser.add_argument('-n', metavar='nails', type=int, default=300, help='number of nails on the board')
+parser.add_argument('-k', metavar='colors', type=int, default=4, help='number of colors of thread')
 parser.add_argument('image_name', help='the filename of the image, including the extension')
-parser.add_argument('color_palette', type=int, help='N hexidecimal numbers representing the different colors of thread used.')
-#no visual output
-#thread multiplier
-#from textfile
+#parser.add_argument('color_palette', nargs='+', type=lambda x: int(x, 0), help='N hexidecimal numbers representing the different colors of thread used.')
 
 args = parser.parse_args(sys.argv[1:])
 
@@ -35,12 +35,23 @@ with Image.open(IMAGE_DIR + args.image_name) as image:
     if image.size[0] != image.size[1]:
         print('Image must be square')
         exit()
-    mask = Image.new('RGB', image.size, (255, 255, 255, 255))
+    mask = Image.new('RGB', image.size, (0, 0, 0))
     draw = ImageDraw.Draw(mask)
-    draw.ellipse([(0, 0), (image.size[0] - 1, image.size[1] - 1)], (0, 0, 0, 0))
-    image = ImageChops.subtract(image, mask)
-    bands = [np.asarray(band) for band in image.split()[::-1]]
+    corners = [(0, 0), (image.size[0] - 1, image.size[1] - 1)]
+    draw.ellipse(corners, (255, 255, 255))
+    image = ImageChops.multiply(image, mask)
+    draw.rectangle(corners, tuple(MASK_COLOR))
+    draw.ellipse(corners, (0, 0, 0))
+    image = ImageChops.add(image, mask)
 
+    data = np.asarray(image).reshape(image.size[0] * image.size[1], 3)
+    data = data[np.all(data != MASK_COLOR, axis=1)]
+
+    km = KMeans(n_clusters=args.k)
+    km.fit(data)
+    print(km.cluster_centers_)
+    
+"""
     band_means = ImageStat.Stat(image).mean[::-1]
     total = sum(band_means)
     band_threads = [int(round(mean / total * args.i)) for mean in band_means]
@@ -85,4 +96,4 @@ with Image.open(IMAGE_DIR + args.image_name) as image:
             file.write(str(nail))
             print(f'{"KYMC"[i]} band done')
 
-    result.save(RESULT_PATH)
+    result.save(RESULT_PATH)"""
