7ac0ce695f6a771b6efc21dab4686ea32edfc2c6
Miles Turin <milesturin@gmail.com>
2021-12-04 19:06:40 -0800

    tweaks

--- seamster.py
+++ seamster.py
@@ -3,6 +3,7 @@ from math import sin, cos, tau
 from PIL import Image, ImageStat, ImageDraw, ImageChops
 import numpy as np
 from bresenham import bresenham
+from functools import cache
 
 IMAGE_DIR = 'images/'
 RESULT_PATH = 'out.jpg'
@@ -10,21 +11,21 @@ INSTRUCTIONS_PATH = 'instructions.txt'
 RESULT_SIZE = 5000
 RESULT_SIZE = (RESULT_SIZE, RESULT_SIZE)
 NEAR_CULL = 10
-THREAD_SUBTRACT = 20
+THREAD_SUBTRACT = 40
 
 def calculate_nail_coord(nail, total_nails, image_size):
     angle = (nail / total_nails) * tau
     calc_coord = lambda f: min(image_size - 1, max(0, int(round(image_size / 2.0 * (1.0 + f(angle))))))
     return (calc_coord(cos), calc_coord(sin))
 
+@cache
+def memo_bresenham(x0, y0, x1, y1):
+    return tuple(bresenham(x0, y0, x1, y1))
+
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', metavar='iterations', type=int, default=5000, help='number of iterations')
 parser.add_argument('-n', metavar='nails', type=int, default=300, help='number of nails on the board')
-parser.add_argument('--ideal', action='store_true', help='allows the algorithm to start from any nail each iteration')
 parser.add_argument('image_name', help='the filename of the image, including the extension')
-#no visual output
-#thread multiplier
-#from textfile
 
 args = parser.parse_args(sys.argv[1:])
 
@@ -39,9 +40,9 @@ with Image.open(IMAGE_DIR + args.image_name) as image:
     draw = ImageDraw.Draw(mask)
     draw.ellipse([(0, 0), (image.size[0] - 1, image.size[1] - 1)], (0, 0, 0, 0))
     image = ImageChops.subtract(image, mask)
-    bands = [np.asarray(band) for band in image.split()[::-1]]
+    bands = [np.asarray(band) for band in image.split()]
 
-    band_means = ImageStat.Stat(image).mean[::-1] #try rms
+    band_means = ImageStat.Stat(image).mean
     total = sum(band_means)
     band_threads = [int(round(mean / total * args.i)) for mean in band_means]
 
@@ -51,13 +52,13 @@ with Image.open(IMAGE_DIR + args.image_name) as image:
     with open(INSTRUCTIONS_PATH, 'w') as file:
         result = Image.new('CMYK', RESULT_SIZE)
         draw = ImageDraw.Draw(result)
-        last_index = 0
-
+        total_threaded = 0
         for i, threads in enumerate(band_threads):
             line_color = [0, 0, 0, 0]
-            line_color[3 - i] = 255
+            line_color[i] = 255
             line_color = tuple(line_color)
-            file.write(f'SWITCH TO {"KYMC"[i]} THREAD\n')
+            file.write(f'SWITCH TO {"CMYK"[i]} THREAD\n')
+
             nail = 0
             for j in range(threads):
                 file.write(f'{nail}, ')
@@ -69,20 +70,20 @@ with Image.open(IMAGE_DIR + args.image_name) as image:
                         continue
                     filled = 0.0
                     pixels = 0
-                    for coord in bresenham(*nail_coords[nail], *nail_coords[k]):
+                    for coord in memo_bresenham(*nail_coords[nail], *nail_coords[k]):
                         filled += bands[i][coord[1]][coord[0]]
                         pixels += 1
                     filled /= pixels
                     if filled > most_filled:
                         best_nail = k
                         most_filled = filled
-                for coord in bresenham(*nail_coords[nail], *nail_coords[best_nail]):
+                for coord in memo_bresenham(*nail_coords[nail], *nail_coords[best_nail]):
                     bands[i][coord[1]][coord[0]] = max(0, bands[i][coord[1]][coord[0]] - THREAD_SUBTRACT)
                 draw.line((result_nail_coords[nail], result_nail_coords[best_nail]), line_color)
                 nail = best_nail
                 if j % 10 == 0:
-                    print(f'{round(j / threads * 100)}%', end='\r')
-            file.write(str(nail))
-            print(f'{"KYMC"[i]} band done')
+                    print(f'{round((j + total_threaded) / args.i * 100, 1)}%', end='\r')
+            file.write(f'{nail}\n')
+            total_threaded += threads
 
     result.save(RESULT_PATH)
