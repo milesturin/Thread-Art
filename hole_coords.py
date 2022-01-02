a90a44eb64b4c48390a16fa0fe243d4a09e29add
Miles Turin <milesturin@gmail.com>
2022-01-01 23:09:16 -0800

    changed coord rounding on hole positioning

---
+++ hole_coords.py
@@ -0,0 +1,10 @@
+from math import sin, cos, pi
+
+DIAMETER = 18
+MARGIN = 0.25
+NAILS = 300
+actual_nails = NAILS // 2
+actual_dist = DIAMETER / 2 - MARGIN
+for i in range(actual_nails):
+    rad = i / actual_nails * pi
+    print(f'{i}: ({round(cos(rad) * actual_dist, 3)}, {round(sin(rad) * actual_dist, 4)})')
