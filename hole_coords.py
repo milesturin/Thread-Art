c087f6734d7c9154f2869ca0b7eda91f21cde436
Miles Turin <milesturin@gmail.com>
2022-01-01 23:10:28 -0800

    fixed rounding again

--- hole_coords.py
+++ hole_coords.py
@@ -7,4 +7,4 @@ actual_nails = NAILS // 2
 actual_dist = DIAMETER / 2 - MARGIN
 for i in range(actual_nails):
     rad = i / actual_nails * pi
-    print(f'{i}: ({round(cos(rad) * actual_dist, 3)}, {round(sin(rad) * actual_dist, 4)})')
+    print(f'{i}: ({round(cos(rad) * actual_dist, 3)}, {round(sin(rad) * actual_dist, 3)')
